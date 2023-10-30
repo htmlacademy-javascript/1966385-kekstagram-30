8058d1045786dc96772b5bbc2d6543b3ff21cbca
Keks <keks@htmlacademy.ru>
2023-10-30 21:37:17 +0300

    :heavy_check_mark: Сборка #5

--- 5/js/util.js
+++ 5/js/util.js
@@ -1,9 +1,9 @@
-import { NAMES, MESSAGE, DESCRIPTION } from 'data.js';
+import { NAMES, MESSAGES, DESCRIPTIONS } from 'data.js';
 
 function getRandomInteger(min, max) {
-  const IntMin = Math.ceil(min);
-  const IntMax = Math.floor(max);
-  return Math.floor(Math.random() * (IntMax - IntMin + 1)) + IntMin;
+  const intMin = Math.ceil(min);
+  const intMax = Math.floor(max);
+  return Math.floor(Math.random() * (intMax - intMin + 1)) + intMin;
 }
 
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
@@ -16,14 +16,14 @@ export { getRandomArrayElement, getRandomInteger, generateIdComment, generateIdF
 const createCommentsArray = () => ({
   commentsId: generateIdComment,
   avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
-  message: MESSAGE[Math.round(Math.random() * (MESSAGE.length - 1))],
+  message: MESSAGES[Math.round(Math.random() * (MESSAGES.length - 1))],
   name: getRandomArrayElement(NAMES),
 });
 
 const createUserPhoto = () => ({
   userId: generateIdForUser,
   urlPhoto: `photos/${generatePhoto}.jpg`,
-  description: getRandomArrayElement(DESCRIPTION),
+  description: getRandomArrayElement(DESCRIPTIONS),
   likes: getRandomInteger(15, 200),
   comments: Array.from({ length: getRandomInteger(0, 30) }, createCommentsArray),
 });
