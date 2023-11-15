bbaf16f000a8e95e9045634e3440fd965bcafb1d
Keks <keks@htmlacademy.ru>
2023-11-15 22:24:59 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/function.js
@@ -0,0 +1,9 @@
+function isLongestString(string, maxLength) {
+  return string.length <= maxLength;
+}
+
+isLongestString('проверяемая строка', 20);
+isLongestString('проверяемая строка', 18);
+isLongestString('проверяемая строка', 10);
+
+//Делу - время
---
+++ 9/js/main.js
@@ -0,0 +1,7 @@
+import './data.js';
+import { generatedPosts } from './util.js';
+import { renderGallery } from './gallery.js';
+
+renderGallery(generatedPosts());
+
+
